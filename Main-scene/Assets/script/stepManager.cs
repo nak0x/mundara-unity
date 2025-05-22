2402792d9eb8522d8031ba053b7917afa2b026a0
math-pixel <59537610+math-pixel@users.noreply.github.com>
2025-05-22 15:03:25 +0200

    WIP Step PANNEL
    NEED TO TEST LOL

--- Main-scene/Assets/script/stepManager.cs
+++ Main-scene/Assets/script/stepManager.cs
@@ -26,6 +26,9 @@ public class StepManager : MonoBehaviour
     private GameObject _currentObject;
     private int _currentGameStep = 0;
     
+    private int _currentPanelStep = 0;
+    private StateStep currentStateStep;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -34,6 +37,9 @@ public class StepManager : MonoBehaviour
         _currentGameStep = 0;
         gameObject.transform.position = workingPlace.gameObject.transform.position; 
 
+        // init STATE Step
+        currentStateStep = StateStep.PannelStep;
+        
         // init Step
         UpdateStep();
     }
@@ -73,6 +79,7 @@ public class StepManager : MonoBehaviour
         if (_currentGameStep + 1 < objectsOfSteps.Count)
         {
             _currentGameStep++;
+            currentStateStep = StateStep.PannelStep;
             UpdateStep();
         }
     }
@@ -85,41 +92,92 @@ public class StepManager : MonoBehaviour
             UpdateStep();
         }
     }
+    
+    public void NextPannelStep()
+    {
+        if (_currentPanelStep + 1 < objectsOfSteps.Count)
+        {
+            _currentPanelStep++;
+            UpdateStep();
+        }
+    }
+
+    public void PreviousPannelStep()
+    {
+        if (_currentPanelStep - 1 >= 0)
+        {
+            _currentPanelStep--;
+            UpdateStep();
+        }
+    }
 
     private void UpdateStep()
     {
         
-        // display presentation pannel
-        if (StepScreenPresentations[_currentGameStep] != null)
-        {
-            ScreenPresentation currentData = StepScreenPresentations[_currentGameStep][0];
-            presentationStepPanel.ShowPanel(currentData.title, currentData.description);
-        }
+        // check if need to display step pannel
         
-        // Display the next Object
-        if (_currentObject != null)
+        // if there is screens to display
+        if (currentStateStep == StateStep.PannelStep)
         {
-            Destroy(_currentObject);
-        }
-        if (objectsOfSteps[_currentGameStep] != null)
+            if (StepScreenPresentations[_currentGameStep] != null)
+            {
+                // check last pannel and if the pannel exits
+                if (_currentPanelStep < StepScreenPresentations[_currentGameStep].Count - 1 && StepScreenPresentations[_currentGameStep][_currentPanelStep] != null)
+                {
+                    // display pannel
+                    ScreenPresentation currentData = StepScreenPresentations[_currentGameStep][_currentPanelStep];
+                    presentationStepPanel.ShowPanel(currentData.title, currentData.description);
+                }
+                else
+                {
+                    // display working UI
+                    currentStateStep = StateStep.WorkingStep;
+                    UpdateStep();
+                }
+            }
+            else
+            {
+                // display working UI
+                currentStateStep = StateStep.WorkingStep;
+                UpdateStep();
+            }
+        }else if (currentStateStep == StateStep.WorkingStep)
         {
-            _currentObject = Instantiate(objectsOfSteps[_currentGameStep], gameObject.transform.position, Quaternion.identity);
-            _currentObject.transform.localScale = new Vector3(0.28f, 0.28f, 0.28f);
-        }
+            // Display the next Object
+            if (_currentObject != null)
+            {
+                Destroy(_currentObject);
+            }
+            if (objectsOfSteps[_currentGameStep] != null)
+            {
+                _currentObject = Instantiate(objectsOfSteps[_currentGameStep], gameObject.transform.position, Quaternion.identity);
+                _currentObject.transform.localScale = new Vector3(0.28f, 0.28f, 0.28f);
+            }
         
-        // Update the progress bar
-        progressBar.updateState(_currentGameStep);
+            // Update the progress bar
+            progressBar.updateState(_currentGameStep);
 
-        // Update narrator
-        narateur.say(textsOfSteps[_currentGameStep]);
+            // Update narrator
+            narateur.say(textsOfSteps[_currentGameStep]);
+            
+            // reset State to pannel for the next step
+            currentStateStep = StateStep.PannelStep;
+        }
+        
         
     }
 }
 
-[System.Serializable]
+[Serializable]
 public class ScreenPresentation
 {
     public String title;
     public String description;
     public GameObject objectOfStep;
+}
+
+enum StateStep
+{
+    PannelStep,
+    WorkingStep
 }
