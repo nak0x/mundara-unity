0fefefc12e521e29e9a25663dce4ef20489ec76f
math-pixel <59537610+math-pixel@users.noreply.github.com>
2025-05-30 13:18:41 +0200

    wip introduction

--- Main-scene/Assets/script/IntroductionManager.cs
+++ Main-scene/Assets/script/IntroductionManager.cs
@@ -1,21 +1,30 @@
 using System.Collections.Generic;
 using DG.Tweening;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class IntroductionManager : MonoBehaviour
 {
     
     // todo create custom class propertie and get a list of properties
+    public int numberOfSteps = 5;
     
     // MundaraTitle
     public RectTransform MundaraTitle;
-    public List<float> mundaraPositions;
-    public List<float> mundaraSize;
+    public List<DataObject> MundaraTileData = new List<DataObject>();
 
     // subtitle
     public RectTransform Subtitle;
-    public List<float> subtitlePositionY;
-    public List<float> subtitleSize;
+    public List<DataObject> SubtitleData = new List<DataObject>();
+    
+    // description text
+    public RectTransform descriptionText;
+    public List<DataObject> DescriptionData = new List<DataObject>();
+
+    // HAnds
+    public GameObject virtualHands;
+    public GameObject reelHands;
     
     public float duration = 1f;
     
@@ -48,19 +57,65 @@ public class IntroductionManager : MonoBehaviour
 
     public void UpdateStep(int step)
     {
+        Debug.Log("Update step : " + step);
         switch (step)
         {
             case 0:
-                MundaraTitle.DOAnchorPosY(mundaraPositions[currentStep], duration);
-                Subtitle.DOAnchorPosY(subtitlePositionY[currentStep], duration);
+                // skip auto to case "1" after x sec
+                // come in the view
+                MundaraTitle.DOAnchorPosY(MundaraTileData[currentStep].positionY, duration);
+                MundaraTitle.DOScale(MundaraTileData[currentStep].size, duration);
+                Subtitle.DOAnchorPosY(SubtitleData[currentStep].positionY, duration);
+                virtualHands.SetActive(false);
                 break;
             case 1:
-                MundaraTitle.DOAnchorPosY(mundaraPositions[currentStep], duration);
-                Subtitle.DOAnchorPosY(subtitlePositionY[currentStep], duration);
+                // skip auto to case "2"
+                // display only the subtitle
+                MundaraTitle.DOScale(MundaraTileData[currentStep].size, duration);
+                Subtitle.DOAnchorPosY(SubtitleData[currentStep].positionY, duration);
+                descriptionText.DOAnchorPosY(DescriptionData[currentStep].positionY, duration);
+                descriptionText.gameObject.GetComponent<TMP_Text>().DOText("", duration);
+                descriptionText.gameObject.GetComponent<TMP_Text>().DOColor(new Color(0,0,0,0), duration);
+                reelHands.transform.DOMove(new Vector3(0.00899999961f,-0.395999998f,-0.63499999f), duration);
                 break;
             case 2:
-                Subtitle.DOAnchorPosY(subtitlePositionY[currentStep], duration);
+                // skip auto after hand detected and place on screen to case "3"
+                // pull up the subtitle and display the description
+                Subtitle.DOAnchorPosY(SubtitleData[currentStep].positionY, duration);
+                descriptionText.gameObject.GetComponent<TMP_Text>().DOColor(Color.white, duration);
+                descriptionText.DOAnchorPosY(DescriptionData[currentStep].positionY, duration);
+                descriptionText.gameObject.GetComponent<TMP_Text>().DOText("Ouvrez votre main droite et bougez la devants le capteur", duration);
+                
+                // play hand
+                reelHands.transform.DOMove(new Vector3(0.00899999961f,0.231999993f,-0.63499999f), duration);
+                
+                // hide virtual hand
+                virtualHands.SetActive(false);
+                
+                break;
+            case 3:
+                // todo hand URP auto change to outline and need to be ghost hand
+                // feedback when the gesture is correcte
+                // tutorial for help guys to swipe
+                // skip when the swipe is detected 2 times
+                descriptionText.gameObject.GetComponent<TMP_Text>().DOText("Voici le geste pour passez les differentes etapes. Essayer de le reproduire.", duration);
+                virtualHands.SetActive(true);
+                break;
+            
+            case 4:
+                
                 break;
         }
     }
+}
+
+[System.Serializable]
+public class DataObject
+{
+    public float positionY = 0;
+    public float positionX = 0;
+    public float width = 0;
+    public float height = 0;
+    public float opacity = 1;
+    public Vector3 size = new Vector2(1, 1);
 }
